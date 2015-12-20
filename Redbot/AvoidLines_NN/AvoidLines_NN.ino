d166b4348d5b3b0853018ecaa06e5af72fac4109
exolain <nmarinpe@gmail.com>
2015-12-20 16:26:17 -0600

    Actualizando codigo de red neuronal

--- Redbot/AvoidLines_NN/AvoidLines_NN.ino
+++ Redbot/AvoidLines_NN/AvoidLines_NN.ino
@@ -4,6 +4,9 @@
 #define SPEED 60  // sets the nominal speed. Set to any number from 0 - 255.
 
 RedBotMotors motors;
+boolean s1 = true;
+boolean s2 = true;
+boolean s3 = true;
 int leftSpeed;   // variable used to store the leftMotor speed
 int rightSpeed;  // variable used to store the rightMotor speed
 RedBotSensor left = RedBotSensor(A3);   // initialize a left sensor object on A3
@@ -85,10 +88,12 @@ perceptron brainR(3);
 
 void setup()
 {
+    Serial.begin(9600);
   randomSeed(analogRead(A3));
   brainL.randomize();
   brainR.randomize();
-  Serial.begin(9600);
+
+
 
 }
 
@@ -96,14 +101,20 @@ void loop()
 {
 
 
+ 
+// if(Serial.available())
+ //{
+  //Serial.println("Goodnight moon!");
   int l = left.read();
   int r = right.read();
   int c = center.read();
   //set inputs
   brainL.inputs[0] = l;
-  brainL.inputs[1] = r;
+  brainR.inputs[1] = c;
+  brainL.inputs[2] = r;
   brainR.inputs[0] = l;
-  brainR.inputs[1] = r;
+  brainR.inputs[1] = c;
+  brainR.inputs[2] = r;
 
   //feed forward and calculate weighted sum
   int resL = brainL.feedForward();
@@ -112,21 +123,25 @@ void loop()
   //use this to move servos left and right
   moveLeft(resL);
   moveRight(resR);
+  Serial.println(resL);
+  //Serial.println(resR);
 
 
   delay(100);//let move for some time then stop
+  stopRedbot();
+  delay(10);
   //train
   brainL.train(eval(l, c, r), resL);
   brainR.train(eval(r, c, l), resR);
 
-
+ //}
 }
 
 
 //***** sensor function******
 
 int eval(int l, int c, int r) {
-   if((l > LINETHRESHOLD) && (c > LINETHRESHOLD) && (r > LINETHRESHOLD) )
+   if ((l > LINETHRESHOLD) && (c > LINETHRESHOLD) && (r > LINETHRESHOLD) )
 	{
             return 1;
 	}
@@ -137,41 +152,35 @@ int eval(int l, int c, int r) {
 
 
 void moveLeft(char d) {
+  leftSpeed = -(SPEED - 50);
   if (d > 0) {
-    motors.leftMotor(0);
+    //Serial.println(d);
+    motors.drive(-80);
+    
+  delay(1000);       // for 1000 ms.
   } else {
-    motors.leftMotor(180);
+    //motors.leftMotor(100);//180
+    //motors.rightMotor(150); // Turn on right motor clockwise medium power (motorPower = 150) 
+    motors.leftDrive(-255);
+    motors.leftMotor(80); // Turn on left motor counter clockwise medium power (motorPower = 150) 
   }
 }
 
 void moveRight(char d) {
+   rightSpeed = SPEED - 50;
   if (d > 0) {
-    motors.rightMotor(180);
+    //motors.rightMotor(-100);//180
+    //motors.drive(-255);
+    motors.rightDrive(-255);
+    motors.rightMotor(80); // Turn on right motor clockwise medium power (motorPower = 150) 
+    //motors.leftMotor(-150); // Turn on left motor counter clockwise medium power (motorPower = 150) 
   } else {
-   motors.rightMotor(0);
+   motors.drive(80);
   }
 }
 
-void moveServo(char d) {
-  if (d > 0) {
-    motors.rightMotor(180);
-    motors.leftMotor(0);
-  } else {
-    motors.rightMotor(0);
-    motors.leftMotor(180);
-  }
-}
 
-void stopServo() {
-  motors.stop();
+void stopRedbot() {
+  motors.brake();
 
 }
-
-void turnRedbot(char d) {
-
-    motors.rightMotor(-100);
-    motors.leftMotor(-100); 
-
-}
-
-
